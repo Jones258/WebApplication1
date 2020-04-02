2a3c3068419b8c374f37a82dc46e196f105e8a3f
董 武港 <1258038895@qq.com>
2020-04-02 11:24:48 +0800

    fssx

---
+++ WebApplication1/Controllers/jjoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class jjoController : Controllers
+    {
+        // GET: jjo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult dds()
+        {
+            return View();
+        }
+    }
+}
